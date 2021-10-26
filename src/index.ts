817afc2232613dd5fca8bcbb3cfb15ef206b528b
tyufjvbn2 <tyufjvbn22@gmail.com>
2021-10-26 13:24:22 +0900

    feat: making file upload done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import nodemailer from "nodemailer";
+import formidable from "formidable";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -15,6 +17,7 @@ router.get("/", (req, res) => {
 	res.send("test server!");
 });
 
+///잘못된 이메일 에러 테스트
 router.post("/test", async (req, res) => {
 	console.log("accept well");
 	const { title, content, email } = req.body;
@@ -52,6 +55,40 @@ router.post("/test", async (req, res) => {
 			});
 		}
 	});
+});
+
+//formidable 파일 전송 테스트
+router.post("/file", async (req, res, next) => {
+	console.log("body", req);
+
+	const targetPath = path.join(__dirname, "../");
+
+	const formOption = {
+		// multiples: true,
+		uploadDir: targetPath + "/uploaded",
+		keepExtensions: true,
+	};
+
+	const form = new formidable.IncomingForm(formOption);
+
+	await form.parse(req, (err, fields, files) => {
+		if (err) {
+			console.log("error occured : ", err);
+			// next(err)
+			res.status(400).json({
+				message: "error",
+			});
+		}
+		res.status(200).json({
+			message: "anyway done",
+			fields,
+			files,
+		});
+	});
+
+	// res.status(200).json({
+	// 	message: "anyway done always",
+	// });
 });
 
 app.listen(process.env.PORT, () => {
